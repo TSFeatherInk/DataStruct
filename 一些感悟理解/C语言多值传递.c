8554adc67ed50e4b90dda9b1cc7cfbe10b859a0b
TSFeatherInk <13759618099@163.com>
2019-12-24 14:56:19 +0800

    感悟

---
+++ 一些感悟理解/C语言多值传递.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+typedef struct{
+	int* a;
+	int* b;
+}Turn;
+Turn* value(){
+	int* a=(int*)malloc(sizeof(int)*3);
+	for(int i=0;i<3;i++){
+		a[i]=i;
+	}
+	int* b=(int*)malloc(sizeof(int)*3);
+	for(int i=0;i<3;i++){
+		b[i]=i+3;
+	}
+	Turn* c = (Turn*)malloc(sizeof(Turn));
+	c->a = a;
+	c->b = b;
+	return c;
+}
+
+int main(){
+	Turn* c = value();
+	int* a = (int*)malloc(sizeof(int)*3);
+	a = c->a;
+	int* b = (int*)malloc(sizeof(int)*3);
+	b = c->b;
+	for(int i=0;i<3;i++){
+		printf("%d ",a[i]);
+	}
+	printf("\n");
+	for(int i=0;i<3;i++){
+		printf("%d ",b[i]);
+	}
+	
+	return 0;
+}
